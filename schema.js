1d49993e30adbbc11e776cbbd223b5e5bf8e0ff6
Benjamin Kyhn <benkyhn@hotmail.com>
2021-02-04 13:21:35 +0100

    custom types

--- schema.js
+++ schema.js
@@ -5,22 +5,40 @@ type Friend {
 id: ID
 firstName: String
 lastName: String
-gender: String
+gender: Gender
 email: String
 age: Int
+contacts: [Contact]
+}
+
+type Contact {
+firstName: String
+lastName: String
+}
+
+enum Gender {
+MALE
+FEMALE
+OTHER
 }
 
 type Query {
-getFriend(id, ID): Friend
+getFriend(id: ID): Friend
 }
 
 input FriendInput{
 id: ID
 firstName: String
 lastName: String
-gender: String
+gender: Gender
 email: String
 age: Int
+contacts: [ContactInput]
+}
+
+input ContactInput {
+firstName: String
+lastName: String
 }
 
 type Mutation {
